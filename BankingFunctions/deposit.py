5b883a8fd5ec499f383a514ec58bac21d9065ce9
Maverick <leonluisf9@outlook.com>
2025-05-18 20:41:42 -0500

    Implementing deposti, withdraw and transfer functions

--- BankingFunctions/deposit.py
+++ BankingFunctions/deposit.py
@@ -2,7 +2,7 @@
 
 # TODO: Build out the handle_deposit function
 # TODO: Pass in the checking account and savings account objects.
-def handle_deposit():
+def handle_deposit(checking, savings):
     """
     This function handles the deposit process for the user.
 
@@ -12,35 +12,48 @@ def handle_deposit():
     """
     print("Which account would you like to make a deposit?")
     # TODO: Prompt the user to select an account and make a deposit.
+    account_choice = input("Enter 1 for checking,\n"
+                           "enter 2 for savings,\n"
+                           "enter q to quit: ") 
     # TODO: If the user chooses to quit, return from the function.
-    if:
+    if account_choice == 'q':
         return
-
     try:
         # TODO: If the selection is in a list of valid choices, i.e ['1', '2']
-        if:
+        if account_choice in ['1', '2']:
             try:
                 # TODO: Prompt the user to enter the amount to deposit and convert it to a float.
-
+                amount = float(input("Enter the amount to deposit: "))
             # Use the ValueError as an exception.
             except ValueError:
                 # TODO: Print an error message if the user enters an invalid amount.
-
+                print("Please enter a dollar amount.\n")
                 # TODO: Call the handle_deposit function recursively for an invalid amount.
-
+                handle_deposit(checking, savings)
                 # TODO: Ensure the function returns after the recursive call.
-
+                return
             # TODO: Add an if/else conditional statement to check the account choice,
-            if:
+            if account_choice == '1':
                 # TODO: Call the withdraw method on the appropriate account.
+                print(f"Depositing ${amount:,.2f} into checking.")
+                checking.deposit(amount)
                 # TODO: Add a print statement to display the updated balance after the deposit
                 # TODO: Format the balance to two decimal places and thousands.
+                checking = checking.get_balance()
+                print("New Balance:")
+                print(f"Checking: ${checking:,.2f}")               
             else:
                 # TODO: Call the deposit methods on the appropriate account.
+                print(f"Depositing ${amount:,.2f} into savings.")
+                savings.deposit(amount)
                 # TODO: Add a print statement to display the updated balance after the deposit
                 # TODO: Format the balance to two decimal places and thousands.
+                savings = savings.get_balance()
+                print("New Balance:")
+                print(f"Savings: ${savings:,.2f}")
         else:
             # TODO: Raise a ValueError with a message stating the user entered an invalid choice.
+            raise ValueError("Invalid choice. Please enter 1, 2, or q.\n")      
     # If the user enters an invalid choice,
     # Print the ValueError message and call the handle_deposit function recursively.
     except ValueError as e:
--- BankingFunctions/transfer.py
+++ BankingFunctions/transfer.py
@@ -1,7 +1,7 @@
 """This function handles the transfer process for the user."""
 
 # TODO: Pass in the checking_account and savings_account objects.
-def handle_transfer():
+def handle_transfer(checking, savings):
     """
     Handles the transfer of funds between checking and savings accounts.
 
@@ -19,29 +19,40 @@ def handle_transfer():
     """
     print("Which account would you like to transfer from?")
     # TODO: Prompt the user to select an account to transfer from.
+    account_choice = input("Enter 1 to transfer from checking to savings,\n"
+                           "enter 2 to transfer from savings to checking,\n"
+                           "enter q to quit: ")
     # TODO: If the user chooses to quit, return from the function.
-    if:
+    if account_choice == 'q':
         return
 
     try:
         # TODO: If the selection is in a list of valid choices, i.e ['1', '2']
-        if:
+        if account_choice in ['1', '2']:
             try:
                 # TODO: Prompt the user to enter the amount to transfer and convert it to a float.
+                amount = float(input("How much would you like to transfer?"))
             except ValueError:
                 # TODO: Print an error message if the user enters an invalid amount.
+                print("Please enter a dollar amount.\n")
                 # TODO: Call the handle_transfer function recursively if the user enters an invalid amount.
+                handle_transfer(checking, savings)
                 return
 
             # TODO: Add an if/else conditional statement to check the account choice,
-            if:
+            if account_choice == '1':
                 # TODO: Call the withdraw and deposit methods on the appropriate account.
+                checking.withdraw(amount)
+                savings.deposit(amount)
             else:
                 # TODO: Call the withdraw and deposit methods on the appropriate account.
+                savings.withdraw(amount)
+                checking.deposit(amount)
             # After the transfer call the balances function with the accounts.
             balances(checking, savings)
         else:
             # TODO: Raise a ValueError with a message stating the user entered an invalid choice.
+            raise ValueError("Invalid choice. Please enter 1, 2, or q.\n")
     except ValueError as e:
         print(e)
         handle_transfer(checking, savings)
